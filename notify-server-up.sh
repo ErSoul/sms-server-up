fbdb831478480110c2f31a13d95cda436169db65
David Colmenares <davidsaul92426@gmail.com>
2023-06-12 17:55:35 -0400

    feat: Added Twilio support. Added help message.

--- notify-server-up.sh
+++ notify-server-up.sh
@@ -4,26 +4,76 @@
 # @copyright: Copyright (C) 2023 David Colmenares
 # @license: 4-Clause BSD
 
-set -eu
+set -e
 
-encode_credentials() {
+help_usage() {
+	echo -e "usage: `basename $0` [--twilio|--telesign] <message>\n"
+	cat << EOF
+Notify server has started.
+
+Argument:
+ message		Message to be sent.
+
+Options:
+
+ -h --help --usage	Show this help message.
+ --twilio		Use the Twilio service API.
+ --telesign		Use the Telesign service API.
+EOF
+	exit 0
+}
+
+telesign_encode_credentials() {
 	echo "${CUSTOMER_ID}:${API_KEY}" | iconv -t utf-8 | base64 -w0 | sed s/K$/=/
 }
 
 main() {
 	cd `dirname $0` # Go to the executable path to load the .env file
 
-	curl -V > /dev/null 2>&1 || (echo "ERROR: curl must be installed" && exit 1)
-	type base64 > /dev/null 2>&1 || (echo "ERROR: base64 must be in you PATH" && exit 1)
-	type iconv > /dev/null 2>&1 || (echo "ERROR: iconv must be in your PATH" && exit 1)
-	test ! -e $PWD/.env && echo "ERROR: you need to create a .env file" && exit 1
+	curl -V > /dev/null 2>&1 || (echo "ERROR: curl must be installed" >&2 && exit 1)
+	type base64 > /dev/null 2>&1 || (echo "ERROR: base64 must be in you PATH" >&2 && exit 1)
+	type iconv > /dev/null 2>&1 || (echo "ERROR: iconv must be in your PATH" >&2 && exit 1)
+	test ! -e $PWD/.env && echo "ERROR: you need to create a .env file" >&2 && exit 1
+	
+	ARGS="$@"
+	
+	while [ $# -gt 0 ]; do
+		case $1 in
+			--telesign)
+				SERVICE="telesign"
+				shift
+				;;
+			--twilio)
+				SERVICE="twilio"
+				shift
+				;;
+			-h|--help|--usage)
+				help_usage
+				;;
+			-*|--*)
+				echo "Invalid option $1"
+				exit 1
+				;;
+			*)
+				POS_ARGS+=("$1") # Will be added to msg body
+				shift
+				;;
+		esac
+	done
+	
+	# Two substring present in string. src: https://unix.stackexchange.com/a/55391/236112
+	echo ${ARGS[@]} | awk 'err=0; /--twilio/ && /--telesign/ && err=1; {exit err}' > /dev/null 2>&1 || (echo "error: must specify twilio or telesign." >&2 && exit 1)
+	echo ${ARGS[@]} | grep -i -e "--telesign" -e "--twilio" > /dev/null 2>&1 || (echo "error: must specify twilio or telesign." >&2 && exit 1)
+	
+	set -- ${POS_ARGS[@]}
 
 	. $PWD/.env # source file
 
-	[ -z $URL ] && echo "ERROR: URL must be setted." && exit 1
-	[ -z $API_KEY ] && echo "ERROR: API_KEY must be setted." && exit 1
-	[ -z $CUSTOMER_ID ] && echo "ERROR: CUSTOMER_ID must be setted." && exit 1
-	[ -z $PHONE_NUMBER ] && echo "ERROR: PHONE_NUMBER must be setted." && exit 1
+	[ -z $URL ] && echo "ERROR: URL must be setted." >&2 && exit 1
+	[ -z $API_KEY ] && echo "ERROR: API_KEY must be setted." >&2 && exit 1
+	[ -z $CUSTOMER_ID ] && echo "ERROR: CUSTOMER_ID must be setted." >&2 && exit 1
+	[ -z $PHONE_TARGET ] && echo "ERROR: PHONE_TARGET must be setted." >&2 && exit 1
+	[ -z $SERVICE ] || [ $SERVICE = 'twilio' ] && [ -z $PHONE_SRC ] && echo "ERROR: PHONE_SRC must be setted." >&2 && exit 1
 
 	cd $OLDPWD # Go back to previous directory
 
@@ -35,14 +85,29 @@ main() {
 		echo 'Waiting for server to be online...'
 	done
 
-	curl --url ${URL} --silent \
-		 --header "authorization: Basic `encode_credentials`" \
-		 --header 'accept: application/json' \
-		 --header 'content-type: application/x-www-form-urlencoded' \
-		 --data is_primary=true \
-		 --data message_type=ARN \
-		 --data "phone_number=${PHONE_NUMBER}" \
-		 --data-urlencode "message=$*"
+	case $SERVICE in
+		telesign)
+			curl --url ${URL} --silent \
+				 --header "authorization: Basic `telesign_encode_credentials`" \
+				 --header 'accept: application/json' \
+				 --header 'content-type: application/x-www-form-urlencoded' \
+				 --data is_primary=true \
+				 --data message_type=ARN \
+				 --data "phone_number=${PHONE_TARGET}" \
+				 --data-urlencode "message=$*"
+			;;
+		twilio)
+			curl --url ${URL} --silent \
+				 --data-urlencode "message=$*" \
+				 --data-urlencode "Body=$*" \
+				 --data-urlencode "From=${PHONE_SRC}" \
+				 --data-urlencode "To=${PHONE_TARGET}" \
+				 -u "${CUSTOMER_ID}:${API_KEY}"
+			;;
+		*)
+			echo "Invalid option: $SERVICE" >&2
+		;;
+	esac
 }
 
 main "$@"
